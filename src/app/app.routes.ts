cc02e406f2c05ddf636b2cd1364010de7080b5d9
Clement <clement2296@gmail.com>
2024-04-21 17:34:32 +0800

    refactor: lazy load routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,12 @@
 import {Routes} from "@angular/router";
-import {ListComponent} from "./features/list/list.component";
-import {DetailComponent} from "./features/detail/detail.component";
 import {pokemonResolver} from "./core/resolvers/pokemon.resolver";
 import {
+  DEFAULT_ERROR_MESSAGE,
   DEFAULT_PATH,
   DETAIL_ROUTE_PARAM,
-  DEFAULT_ERROR_MESSAGE,
   ERROR_PATH,
   NAVIGATE_STATE_ERROR_MESSAGE
 } from "./core/constants/app";
-import {ErrorComponent} from "./core/components/error/error.component";
 
 export const routes: Routes = [
   {
@@ -19,21 +16,21 @@ export const routes: Routes = [
   },
   {
     path: DEFAULT_PATH,
-    component: ListComponent
+    loadComponent: () => import("./features/list/list.component").then(mod => mod.ListComponent)
   },
   {
     path: `${DEFAULT_PATH}/:${DETAIL_ROUTE_PARAM}`,
     resolve: {
       pokemon: pokemonResolver
     },
-    component: DetailComponent
+    loadComponent: () => import("./features/detail/detail.component").then(mod => mod.DetailComponent)
   },
   {
     path: ERROR_PATH,
-    component: ErrorComponent,
     resolve: {
       [NAVIGATE_STATE_ERROR_MESSAGE]: () => DEFAULT_ERROR_MESSAGE,
-    }
+    },
+    loadComponent: () => import("./core/components/error/error.component").then(mod => mod.ErrorComponent)
   },
   {
     path: "**",
